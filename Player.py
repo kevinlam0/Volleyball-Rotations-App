c12b5e415688d2f8025fbcb615b21bf9df801a75
kevinlam0 <kevinclam02@gmail.com>
2023-09-21 19:56:16 -0400

    Added subbing to each player

--- Player.py
+++ Player.py
@@ -1,14 +1,43 @@
 class Player:
     position: str
     name: str
+    row: str
+    sub = None
     
-    def __init__(self, name: str, pos: str):
+    def __init__(self, name: str, pos: str, row: str, sub = None, lib = False):
         self.name = name
         self.position = pos
+        
+        if "both" in row.lower():
+            self.row = "Both"
+        elif "front" in row.lower():
+            self.row = "Front"
+        elif "back" in row.lower():
+            self.row = "Back"
+        
+        if not row == "Both" and sub == None and not lib:
+            self.findSub(row)
+        else:
+            self.sub = sub
+        
     
     def getName(self):
         return self.name
     
     def getPosition(self):
         return self.position
+    
+    def findSub(self, row):
+        if row == "Front":
+            self.sub = Player(input("Who is " + self.name + "'s back-row sub?: "), "DS", "Back", sub = self)
+            
+        elif row == "Back":
+            self.sub = Player(input("Who is " + self.name + "'s front-row sub?: "), input("What is this player's position?: "), "Front", sub = self)
+    
+    def getSub(self):
+        
+        return self.sub
+    
+    def getRow(self):
+        return self.row
     
--- Volleyball.py
+++ Volleyball.py
@@ -18,15 +18,25 @@ class Volleyball_Rotations_Generator:
         Fills in the quadrants defaulted at rotation 1.
         Gets the max length of the names for formatting reasons. 
         """
-        self.setter, self.oh1, self.oh2 = Player(input("Who is your starting Setter?: "), "S"), Player(input("Who is your starting Outside Hitter 1?: "), "OH1"), Player(input("Who is your starting Outside Hitter 2?: "), "OH2")
-        self.lib, self.mb1, self.mb2, self.rs = Player(input("Who is your starting Libero?: "),"L") , Player(input("Who is your starting Middle Blocker 1?: "), "MB1"), Player(input("Who is your starting Middle Blocker 2?: "), "MB2"), Player(input("Who is your starting Right Side?: "), "RS")
+        
+        self.setter= Player(name = input("Who is your starting Setter?: "), pos = "S", row = input("Will this player play back-row (\"Back\"), front-row (\"Front\"), or both (\"Both\")?: "))
+        self.rs = Player(name = input("Who is your starting Right Side?: "), pos = "RS", row = input("Will this player play back-row (\"Back\"), front-row (\"Front\"), or both (\"Both\")?: "))
+        
+        self.oh1 = Player(name = input("Who is your starting Outside Hitter 1?: "), pos = "OH1", row = input("Will this player play back-row (\"Back\"), front-row (\"Front\"), or both (\"Both\")?: "))
+        self.oh2 = Player(name = input("Who is your starting Outside Hitter 2?: "), pos= "OH2", row = input("Will this player play back-row (\"Back\"), front-row (\"Front\"), or both (\"Both\")?: "))
+        
+        self.lib = Player(name = input("Who is your starting Libero?: "),pos = "L", row = "Back", lib = True)
+        self.mb1 = Player(name = input("Who is your starting Middle Blocker 1?: "), pos = "MB1", row = "Front", lib = True)
+        self.mb2 = Player(name = input("Who is your starting Middle Blocker 2?: "), pos = "MB2", row = "Front", lib = True)
+        
         self.quadrants = {"Q1": self.setter, "Q2": self.oh1, "Q3": self.mb2, "Q4": self.rs, "Q5": self.oh2, "Q6": self.lib}
         self.sittingMiddle = self.mb1
-
-        word_list = [self.setter.getName(), self.oh1.getName(), self.oh2.getName(), self.rs.getName(), self.mb1.getName(), self.mb2.getName(), self.lib.getName()]
-        for word in word_list:
-            if len(word) > self.maxLengthOfName:
-                self.maxLengthOfName = len(word)
+        
+    
+        # word_list = [self.setter.getName(), self.oh1.getName(), self.oh2.getName(), self.rs.getName(), self.mb1.getName(), self.mb2.getName(), self.lib.getName()]
+        # for word in word_list:
+        #     if len(word) > self.maxLengthOfName:
+        #         self.maxLengthOfName = len(word)
         
     
     def rotate(self):
@@ -40,25 +50,50 @@ class Volleyball_Rotations_Generator:
             self.quadrants["Q" + str(i)] = self.quadrants.get("Q" + str(i + 1))
         # Put the first person in the missing quadrants
         self.quadrants["Q6"] = first
+        
+        # Adds libero logic
         if self.quadrants.get("Q4").getPosition() == "L":
             self.quadrants["Q4"] = self.sittingMiddle
             self.sittingMiddle = self.quadrants.get("Q1")
             self.quadrants["Q1"] = self.lib
+    
+        
+        elif self.quadrants.get("Q1").getRow() == "Front":
+            sub: Player = self.quadrants.get("Q1").getSub()
+            self.quadrants["Q1"] = sub
+            
+        elif self.quadrants.get("Q4").getRow() == "Back":
+            sub: Player = self.quadrants.get("Q4").getSub()
+            self.quadrants["Q4"] = self.quadrants.get("Q4").getSub()
         
     def makeHomeRotation(self):
         ans = []
         count = 6
         
         while count > 0:
-            frontRow = (self.quadrants.get("Q4"), self.quadrants.get("Q3"), self.quadrants.get("Q2"))
-            backRow = (self.quadrants.get("Q5"), self.quadrants.get("Q6"), self.quadrants.get("Q1"))
-            frontString = "|  {a}  |  {b}  |  {c}  |".format(a = frontRow[0].getName(), b = frontRow[1].getName(), c= frontRow[2].getName())
-            backString = "|  {a}  |  {b}  |  {c}  |".format(a = backRow[0].getName(), b = backRow[1].getName(), c = backRow[2].getName())
+            try:
+                frontRow = (self.quadrants.get("Q4"), self.quadrants.get("Q3"), self.quadrants.get("Q2"))
+                backRow = (self.quadrants.get("Q5"), self.quadrants.get("Q6"), self.quadrants.get("Q1"))
+                frontString = "|  {a}  |  {b}  |  {c}  |".format(a = frontRow[0].getName(), b = frontRow[1].getName(), c= frontRow[2].getName())
+                backString = "|  {a}  |  {b}  |  {c}  |".format(a = backRow[0].getName(), b = backRow[1].getName(), c = backRow[2].getName())
+            
+            except:
+                print(backString)
+                print("Something")
             ans.append((frontString, backString))
             self.rotate()
+    
             count -= 1
             
-        return ans 
+        count = 1
+        for rotation in ans:
+            frontRow, backRow = rotation
+            print("Rotation {a}".format(a = count))
+            print(frontRow)
+            print(backRow)
+            print("\n")
+            count += 1
+            
         
         
 if __name__ == "__main__":
@@ -66,15 +101,9 @@ if __name__ == "__main__":
     # print("|  Name  |  helllo jds  |  fdsjkfls  |")
     generator.inputPlayers()
     
-    rotations = generator.makeHomeRotation()
-    count = 1
-    for rotation in rotations:
-        frontRow, backRow = rotation
-        print("Rotation {a}".format(a = count))
-        print(frontRow)
-        print(backRow)
-        print("\n")
-        count += 1
+    generator.makeHomeRotation()
+    # print(generator.quadrants.get("Q1").getPosition())
+    
     
     
     
