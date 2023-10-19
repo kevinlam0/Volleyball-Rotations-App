4b81d7165de2f9376efc1592f4651bf7641fc85f
kevinlam0 <kevinclam02@gmail.com>
2023-10-19 10:40:54 -0400

    update

--- src/Main.py
+++ src/Main.py
@@ -2,5 +2,5 @@ from Volleyball_Rotation_Generator import Volleyball_Rotations_Generator
 if __name__ == "__main__":
     generator = Volleyball_Rotations_Generator()
     generator.run_menu()
-    generator.ending_message()
+    generator.end()
     
