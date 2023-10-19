5cb342e8370551bced46585a5317f85156b0aecf
kevinlam0 <kevinclam02@gmail.com>
2023-10-18 23:12:33 -0400

    Made argument handling to their own private method

--- Player.py
+++ Player.py
@@ -1,28 +1,16 @@
+VALID_POSITIONS = {'S', 'RS', 'OH1', 'OH2', 'L', 'MB1', 'MB2', 'DS'} 
+VALID_ROW = {'back', 'front', 'both'}
 class Player:
     position: str
     name: str
     row: str
     sub = None
-    POSITIONS = {'S', 'RS', 'OH1', 'OH2', 'L', 'MB1', 'MB2', 'DS'} 
+    
     
     def __init__(self, name: str, pos: str, row: str, sub = None, lib = False):
-        # Name handling
-        while name == "":
-            name = input("The name of this player cannot be blank. Please input a name: ")
-        self.name = name.capitalize()
-        
-        # Position handling
-        playersPosition = pos.upper()
-        while (playersPosition not in self.POSITIONS):
-            playersPosition = input("Please input a valid position (S, OH1, OH2, RS, L, MB1, MB2): ").upper()
-        self.position = playersPosition
-        
-        # Row handling 
-        row = row.lower()
-        while row != 'back' and row != 'front' and row != 'both':
-            row = input("The row input was not valid. Please provide a row (Front, Back, Both): ")
-        
-        self.row = row.capitalize()  
+        self.name = _handle_name(name)
+        self.position = _handle_position(pos)
+        self.row = _handle_row(row)
         
         # Look for sub if the player does not play both rows
         if not self.row == "Both" and sub == None and not lib:
@@ -61,3 +49,20 @@ class Player:
     def deleteSub(self):
         self.sub = None
         self.row = "Both"
+    
+def _handle_name(name: str) -> str:
+    while name.strip() == "":
+        name = input("The name of this player cannot be blank. Please input a name: ")
+    return name.capitalize()
+
+def _handle_position(pos: str) -> str:
+    playersPosition = pos.upper()
+    while (playersPosition not in VALID_POSITIONS):
+        playersPosition = input("Please input a valid position (S, OH1, OH2, RS, L, MB1, MB2): ").upper()
+    return playersPosition
+
+def _handle_row(row: str) -> str:
+    row = row.lower()
+    while row not in VALID_ROW:
+        row = input("The row input was not valid. Please provide a row (Front, Back, Both): ").lower()
+    return row.capitalize()
